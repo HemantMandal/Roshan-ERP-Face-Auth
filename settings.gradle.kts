c5c116f4076bb47aecb8ad0f1affe074174f31d7
HemantMandal <45534916+HemantMandal@users.noreply.github.com>
2026-07-21 18:53:05 +0530

    Configure plugin and dependency resolution management

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
 
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Roshan-ERP-Face-Auth"
+include(":app")
